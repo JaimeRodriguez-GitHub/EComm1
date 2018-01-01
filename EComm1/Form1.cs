70f7fd10fd9325d52517f90fbfed7fd9deec1734
JaimeRodriguez-GitHub <jaimerodriguezvillalta@hotmail.com>
2018-01-01 13:03:36 -0800

    First change

--- EComm1/Form1.cs
+++ EComm1/Form1.cs
@@ -16,5 +16,10 @@ namespace EComm1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
